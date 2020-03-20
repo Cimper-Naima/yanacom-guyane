dc8445689edcbd2d662c19c0811766c2e1ed27ce
Cimper-Naima <cimper.naima973@gmail.com>
2020-03-20 12:47:15 -0300

    Add A Propos

---
+++ Thème1/index.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified Bootstrap CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<link href="style.css" rel="stylesheet">
+    <title>Thème 1</title>
+</head>
+<body>
+
+<header>
+
+    <div class="container-fluid">
+
+<!-- Nav-Bar -->
+<nav class="navbar fixed navbar-expand-sm navbar-light bg-faded">
+
+<!-- Bouton-Nav -->
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<!-- Brand -->
+<a class="navbar-brand" href="index.php">Thème 1</a>
+
+<!-- Links -->
+<div class="collapse navbar-collapse justify-content-end" id="nav-content">   
+<ul class="navbar-nav">
+<li class="nav-item">
+<a class="nav-link" href="index.php">a propos</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="domaines-compétences.php">domaines de compétences</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="equipes.php">équipes</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="contact.php">contact</a>
+</li>
+</ul>
+</div>
+</nav>
+
+<!-- Première ranger -->
+<a href="https://placeholder.com"><img src="https://via.placeholder.com/1485x495.png?text=Image+Générique"></a>
+
+</header>
+
+<!-- A propos -->
+<main>
+<section>
+
+    <div class="col-sm-12">
+
+        <h1>a propos</h1>
+            <p>En droit, l'avocat est un juriste dont les fonctions traditionnelles sont de conseiller, représenter, d'assister et de défendre ses clients, personnes physiques ou morales, en justice, en plaidant pour faire valoir leurs droits et, plus généralement, pour les représenter. Par ailleurs, l'avocat s'acquitte d'une fonction de conseil mais aussi de rédacteur d'actes. En droit, l'avocat est un juriste dont les fonctions traditionnelles sont de conseiller, représenter, d'assister et de défendre ses clients, personnes physiques ou morales, en justice, en plaidant pour faire valoir leurs droits et, plus généralement, pour les représenter. Par ailleurs, l'avocat s'acquitte d'une fonction de conseil mais aussi de rédacteur d'actes.En droit, l'avocat est un juriste dont les fonctions traditionnelles sont de conseiller, représenter, d'assister et de défendre ses clients, personnes physiques ou morales, en justice, en plaidant pour faire valoir leurs droits et, plus généralement, pour les représenter. Par ailleurs, l'avocat s'acquitte d'une fonction de conseil mais aussi de rédacteur d'actes.En droit, l'avocat est un juriste dont les fonctions traditionnelles sont de conseiller, représenter, d'assister et de défendre ses clients, personnes physiques ou morales, en justice, en plaidant pour faire valoir leurs droits et, plus généralement, pour les représenter. Par ailleurs, l'avocat s'acquitte d'une fonction de conseil mais aussi de rédacteur d'actes.</p>
+            <p>Les avocats représentent et défendent leurs clients dans des affaires aussi diverses qu’un divorce, une succession, un litige, un vol, un crime, etc. Et, de fait, certains avocats se spécialisent : avocat d'affaires, avocat en droit du travail, avocat en droit de la famille. Les avocats représentent et défendent leurs clients dans des affaires aussi diverses qu’un divorce, une succession, un litige, un vol, un crime, etc. Et, de fait, certains avocats se spécialisent : avocat d'affaires, avocat en droit du travail, avocat en droit de la famille. Les avocats représentent et défendent leurs clients dans des affaires aussi diverses qu’un divorce, une succession, un litige, un vol, un crime, etc. Et, de fait, certains avocats se spécialisent : avocat d'affaires, avocat en droit du travail, avocat en droit de la famille. Les avocats représentent et défendent leurs clients dans des affaires aussi diverses qu’un divorce, une succession, un litige, un vol, un crime, etc. Et, de fait, certains avocats se spécialisent : avocat d'affaires, avocat en droit du travail, avocat en droit de la famille.  </p>
+    </div>
+    
+    <!-- TimeLine -->
+    <h2>historiques</h2>
+
+<div class="container py-2 mt-4 mb-4">
+   <!-- timeline item 1 -->
+  <div class="row no-gutters">
+    <div class="col-sm"> <!--spacer--> </div>
+    <!-- timeline item 1 center dot -->
+    <div class="col-sm-1 text-center flex-column d-none d-sm-flex">
+      <div class="row h-50">
+        <div class="col">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+      <h5 class="m-2">
+        <span class="badge badge-pill bg-light border">&nbsp;</span>
+      </h5>
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+    </div>
+    <!-- timeline item 1 event content -->
+    <div class="col-sm py-2">
+      <div class="card">
+        <div class="card-body">
+          <div class="float-right text-muted small">Jan 9th 2019 7:00 AM</div>
+          <h4 class="card-title text-muted">Day 1 Orientation</h4>
+          <p class="card-text">Welcome to the campus, introduction and get started with the tour.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/row-->
+  <!-- timeline item 2 -->
+  <div class="row no-gutters">
+    <div class="col-sm py-2">
+      <div class="card border-success shadow">
+        <div class="card-body">
+          <div class="float-right text-success small">Jan 10th 2019 8:30 AM</div>
+          <h4 class="card-title text-success">Day 2 Sessions</h4>
+          <p class="card-text">Sign-up for the lessons and speakers that coincide with your course syllabus. Meet and greet with instructors.</p>
+          <div class="collapse border" id="t22_details">
+            <div class="p-2 text-monospace">
+              <div>08:30 - 09:00 Breakfast in CR 2A</div>
+              <div>09:00 - 10:30 Live sessions in CR 3</div>
+              <div>10:30 - 10:45 Break</div>
+              <div>10:45 - 12:00 Live sessions in CR 3</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-1 text-center flex-column d-none d-sm-flex">
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+      <h5 class="m-2">
+        <span class="badge badge-pill bg-success">&nbsp;</span>
+      </h5>
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+    </div>
+    <div class="col-sm"> <!--spacer--> </div>
+  </div>
+  <!--/row-->
+  <!-- timeline item 3 -->
+  <div class="row no-gutters">
+    <div class="col-sm"> <!--spacer--> </div>
+    <div class="col-sm-1 text-center flex-column d-none d-sm-flex">
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+      <h5 class="m-2">
+        <span class="badge badge-pill bg-light border">&nbsp;</span>
+      </h5>
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+    </div>
+    <div class="col-sm py-2">
+      <div class="card">
+        <div class="card-body">
+          <div class="float-right text-muted small">Jan 11th 2019 8:30 AM</div>
+          <h4 class="card-title">Day 3 Sessions</h4>
+          <p>Shoreditch vegan artisan Helvetica. Tattooed Codeply Echo Park Godard kogi, next level irony ennui twee squid fap selvage. Meggings flannel Brooklyn literally small batch, mumblecore PBR try-hard kale chips. Brooklyn vinyl lumbersexual
+            bicycle rights, viral fap cronut leggings squid chillwave pickled gentrify mustache.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/row-->
+  <!-- timeline item 4 -->
+  <div class="row no-gutters">
+    <div class="col-sm py-2">
+      <div class="card">
+        <div class="card-body">
+          <div class="float-right text-muted small">Jan 12th 2019 11:30 AM</div>
+          <h4 class="card-title">Day 4 Wrap-up</h4>
+          <p>Join us for lunch in Bootsy's cafe across from the Campus Center.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-1 text-center flex-column d-none d-sm-flex">
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+      <h5 class="m-2">
+        <span class="badge badge-pill bg-light border">&nbsp;</span>
+      </h5>
+      <div class="row h-50">
+        <div class="col">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+    </div>
+    <div class="col-sm"> <!--spacer--> </div>
+  </div>
+  <!--/row-->
+</div>
+
+<hr>
+
+<div class="container py-2 mt-4 mb-4">
+  <!-- timeline item 1 -->
+  <div class="row">
+    <!-- timeline item 1 left dot -->
+    <div class="col-auto text-center flex-column d-none d-sm-flex">
+      <div class="row h-50">
+        <div class="col">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+      <h5 class="m-2">
+        <span class="badge badge-pill bg-light border">&nbsp;</span>
+      </h5>
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+    </div>
+    <!-- timeline item 1 event content -->
+    <div class="col py-2">
+      <div class="card">
+        <div class="card-body">
+          <div class="float-right text-muted">Mon, Jan 9th 2019 7:00 AM</div>
+          <h4 class="card-title text-muted">Day 1 Orientation</h4>
+          <p class="card-text">Welcome to the campus, introduction and get started with the tour.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/row-->
+  <!-- timeline item 2 -->
+  <div class="row">
+    <div class="col-auto text-center flex-column d-none d-sm-flex">
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+      <h5 class="m-2">
+        <span class="badge badge-pill bg-success">&nbsp;</span>
+      </h5>
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+    </div>
+    <div class="col py-2">
+      <div class="card border-success shadow">
+        <div class="card-body">
+          <div class="float-right text-success">Tue, Jan 10th 2019 8:30 AM</div>
+          <h4 class="card-title text-success">Day 2 Sessions</h4>
+          <p class="card-text">Sign-up for the lessons and speakers that coincide with your course syllabus. Meet and greet with instructors.</p>
+          <div class="collapse border" id="t2_details">
+            <div class="p-2 text-monospace">
+              <div>08:30 - 09:00 Breakfast in CR 2A</div>
+              <div>09:00 - 10:30 Live sessions in CR 3</div>
+              <div>10:30 - 10:45 Break</div>
+              <div>10:45 - 12:00 Live sessions in CR 3</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/row-->
+  <!-- timeline item 3 -->
+  <div class="row">
+    <div class="col-auto text-center flex-column d-none d-sm-flex">
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+      <h5 class="m-2">
+        <span class="badge badge-pill bg-light border">&nbsp;</span>
+      </h5>
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+    </div>
+    <div class="col py-2">
+      <div class="card">
+        <div class="card-body">
+          <div class="float-right text-muted">Wed, Jan 11th 2019 8:30 AM</div>
+          <h4 class="card-title">Day 3 Sessions</h4>
+          <p>Shoreditch vegan artisan Helvetica. Tattooed Codeply Echo Park Godard kogi, next level irony ennui twee squid fap selvage. Meggings flannel Brooklyn literally small batch, mumblecore PBR try-hard kale chips. Brooklyn vinyl lumbersexual
+            bicycle rights, viral fap cronut leggings squid chillwave pickled gentrify mustache. 3 wolf moon hashtag church-key Odd Future. Austin messenger bag normcore, Helvetica Williamsburg sartorial tote bag distillery Portland before
+            they sold out gastropub taxidermy Vice.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/row-->
+  <!-- timeline item 4 -->
+  <div class="row">
+    <div class="col-auto text-center flex-column d-none d-sm-flex">
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+      <h5 class="m-2">
+        <span class="badge badge-pill bg-light border">&nbsp;</span>
+      </h5>
+      <div class="row h-50">
+        <div class="col">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+    </div>
+    <div class="col py-2">
+      <div class="card">
+        <div class="card-body">
+          <div class="float-right text-muted">Thu, Jan 12th 2019 11:30 AM</div>
+          <h4 class="card-title">Day 4 Wrap-up</h4>
+          <p>Join us for lunch in Bootsy's cafe across from the Campus Center.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/row-->
+</div>
+<!--container-->
+</section>
+</main>
+</body>
+</html>
