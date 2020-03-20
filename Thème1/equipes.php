035857a0d9050a101e5be47c30ca5adfb15bf745
Cimper-Naima <cimper.naima973@gmail.com>
2020-03-20 15:00:01 -0300

    Add-Équipes

--- Thème1/equipes.php
+++ Thème1/equipes.php
@@ -6,11 +6,159 @@
     <!-- Latest compiled and minified Bootstrap CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Document</title>
+    <title>Équipes</title>
 </head>
 <body>
 <?php
 include('header.php');
 ?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-6">
+        <div class="card" style="max-width: 602px;">
+
+<!-- Image -->
+<img class="card-img-top" src="/pix/samples/12s.jpg" alt="Photo of sunset">
+
+<!-- Text Content -->
+<div class="card-body">
+<p class="card-text">Well it was good while it lasted...</p>
+</div>
+
+</div>
+        </div>
+        <div class="col-sm-6">
+        <div class="card" style="max-width: 602px;">
+
+<!-- Image -->
+<img class="card-img-top" src="/pix/samples/12s.jpg" alt="Photo of sunset">
+
+<!-- Text Content -->
+<div class="card-body">
+<p class="card-text">Well it was good while it lasted...</p>
+</div>
+
+</div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+        <div class="card" style="max-width: 602px;">
+
+<!-- Image -->
+<img class="card-img-top" src="/pix/samples/12s.jpg" alt="Photo of sunset">
+
+<!-- Text Content -->
+<div class="card-body">
+<p class="card-text">Well it was good while it lasted...</p>
+</div>
+
+</div>
+        </div>
+        <div class="col-sm-6">
+        <div class="card" style="max-width: 602px;">
+
+<!-- Image -->
+<img class="card-img-top" src="/pix/samples/12s.jpg" alt="Photo of sunset">
+
+<!-- Text Content -->
+<div class="card-body">
+<p class="card-text">Well it was good while it lasted...</p>
+</div>
+
+</div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+        <div class="card" style="max-width: 602px;">
+
+<!-- Image -->
+<img class="card-img-top" src="/pix/samples/12s.jpg" alt="Photo of sunset">
+
+<!-- Text Content -->
+<div class="card-body">
+<p class="card-text">Well it was good while it lasted...</p>
+</div>
+
+</div>
+        </div>
+        <div class="col-sm-6">
+        <div class="card" style="max-width: 602px;">
+
+<!-- Image -->
+<img class="card-img-top" src="/pix/samples/12s.jpg" alt="Photo of sunset">
+
+<!-- Text Content -->
+<div class="card-body">
+<p class="card-text">Well it was good while it lasted...</p>
+</div>
+
+</div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+        <div class="card" style="max-width: 602px;">
+
+<!-- Image -->
+<img class="card-img-top" src="/pix/samples/12s.jpg" alt="Photo of sunset">
+
+<!-- Text Content -->
+<div class="card-body">
+<p class="card-text">Well it was good while it lasted...</p>
+</div>
+
+</div>
+        </div>
+        <div class="col-sm-6">
+        <div class="card" style="max-width: 602px;">
+
+<!-- Image -->
+<img class="card-img-top" src="/pix/samples/12s.jpg" alt="Photo of sunset">
+
+<!-- Text Content -->
+<div class="card-body">
+<p class="card-text">Well it was good while it lasted...</p>
+</div>
+
+</div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+        <div class="card" style="max-width: 602px;">
+
+<!-- Image -->
+<img class="card-img-top" src="/pix/samples/12s.jpg" alt="Photo of sunset">
+
+<!-- Text Content -->
+<div class="card-body">
+<p class="card-text">Well it was good while it lasted...</p>
+</div>
+
+</div>
+        </div>
+        <div class="col-sm-6">
+        <div class="card" style="max-width: 602px;">
+
+<!-- Image -->
+<img class="card-img-top" src="/pix/samples/12s.jpg" alt="Photo of sunset">
+
+<!-- Text Content -->
+<div class="card-body">
+<p class="card-text">Well it was good while it lasted...</p>
+</div>
+
+</div>
+        </div>
+    </div>
+</div>
+
+<?php
+include('footer.php');
+?>
+
 </body>
 </html>
